daeef41da04e9342c8595c530fd0764fa9d1e2f5
Nishanth (Contractor) Thakkar <c-nthakkar@dovercorp.com>
2021-11-18 13:07:59 +0530

    Added default value attribute

--- TicketingAppCloud/DBModels/Ticket.cs
+++ TicketingAppCloud/DBModels/Ticket.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -19,14 +20,18 @@ namespace TicketingAppCloud.DBModels
         public string Details { get; set; }
         [Required]
         [StringLength(16)]
+        [DefaultValue("Medium")]
         public string Priority { get; set; }
         [Required]
         [StringLength(16)]
+        [DefaultValue("Assigned")]
         public string Status { get; set; }
         public string Notes { get; set; }
         [Required]
         [StringLength(16)]
         public string AssignedTo { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [DefaultValue("getdate()")]
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
 
